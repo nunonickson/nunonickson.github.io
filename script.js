bbe6ce738635dd3714c87c63ec7ee34f999e8f49
Nuno Nickson <nunonickson@users.noreply.github.com>
2025-06-14 07:25:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const myButton = document.getElementById('myButton');
-
-    if (myButton) {
-        myButton.addEventListener('click', function() {
-            alert('Tombol ini berhasil diklik!');
-        });
-    }
-});
